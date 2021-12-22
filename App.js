0baed6bf36da9df4d09e7972d53848cf82680f64
Dinaisbaih <isbaihdina.f@outlook.com>
2021-12-22 12:35:37 +0200

    Updated App.js with Rootnavigation

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import RootNavigator from "./src/Navigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  //Font
+  const [loaded] = useFonts({
+    Montserrat: require("./assets/fonts/Montserrat-Regular.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
